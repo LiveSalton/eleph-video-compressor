659a4254ab55f19867b3e3abed6bb546c1aad34c
wujinsheng1 <wujinsheng1@yy.com>
2022-02-04 21:34:11 +0800

    update

--- corelite/src/main/java/com/salton123/base/BaseActivity.kt
+++ corelite/src/main/java/com/salton123/base/BaseActivity.kt
@@ -1,6 +1,8 @@
 package com.salton123.base
 
 import android.os.Bundle
+import android.view.View
+import android.widget.LinearLayout
 import android.widget.TextView
 import com.salton123.base.feature.ImmersionFeature
 import com.salton123.corelite.R
@@ -15,6 +17,7 @@ abstract class BaseActivity : DelegateActivity() {
     private lateinit var mImmersionFeature: ImmersionFeature
     private lateinit var tvBack: TextView
     private lateinit var tvMore: TextView
+    private lateinit var llContent: LinearLayout
     override fun initVariable(savedInstanceState: Bundle?) {
         mImmersionFeature = ImmersionFeature(this)
         addFeature(mImmersionFeature)
@@ -25,6 +28,8 @@ abstract class BaseActivity : DelegateActivity() {
     }
 
     override fun initViewAndData() {
+        llContent = findViewById(R.id.llContent)
+        View.inflate(this, getLayoutId(), llContent)
         tvBack = findViewById(R.id.tvBack)
         tvMore = findViewById(R.id.tvMore)
         tvBack.setOnClickListener {
