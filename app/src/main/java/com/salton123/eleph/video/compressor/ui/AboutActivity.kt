5487eef7f69d858e113af6452b26b591c5cd48bd
wujinsheng1 <wujinsheng1@yy.com>
2022-02-18 17:01:38 +0800

    update ui

--- app/src/main/java/com/salton123/eleph/video/compressor/ui/AboutActivity.kt
+++ app/src/main/java/com/salton123/eleph/video/compressor/ui/AboutActivity.kt
@@ -1,5 +1,6 @@
 package com.salton123.eleph.video.compressor.ui
 
+import android.widget.ImageButton
 import android.widget.TextView
 import com.salton123.base.BaseActivity
 import com.salton123.eleph.BuildConfig
@@ -15,15 +16,20 @@ class AboutActivity : BaseActivity() {
     private lateinit var tvVersion: TextView
     private lateinit var tvBuildHash: TextView
     private lateinit var tvBuildTime: TextView
+    private lateinit var btnClose: ImageButton
     override fun initViewAndData() {
         super.initViewAndData()
         tvVersion = f(R.id.tvVersion)
         tvBuildHash = f(R.id.tvBuildHash)
         tvBuildTime = f(R.id.tvBuildTime)
+        btnClose = f(R.id.btnClose)
         tvTitle.text = getString(R.string.setting_about)
         tvVersion.text = BuildConfig.VERSION_NAME
         tvBuildHash.text = BuildConfig.GIT_HASH
         tvBuildTime.text = BuildConfig.BUILD_TIME
+        btnClose.setOnClickListener {
+            finish()
+        }
     }
 
     override fun getLayoutId(): Int = R.layout.activity_about
